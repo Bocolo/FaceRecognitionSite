b10b37ba65078ffdca284fbc48714064bc68f48a
bocolo <bronaghcolt@hotmail.com>
2021-01-29 20:00:58 +0000

    tidyup
    cleared profile by adding inital state
    
    to do:
    update sigin /register with form component to prevent duplicte code

--- src/App.js
+++ src/App.js
@@ -35,27 +35,32 @@ const particleOptions = {
             }
         }
         
-    }}
+    }
+}
+
+const initialState = {
+    
+    input: '',
+        imageUrl: '',
+            box: { },
+    route: 'signin',
+        isSignedIn: false,
+            user: {
+
+        id: '',
+            name: '',
+
+                email: '',
+                    entries: 0,
+                        joined: ''
+
+    }
+}
+
 class App extends Component {
     constructor() {
         super();
-        this.state= {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-
-                id: '',
-                name: '',
-                
-                email: '',
-                entries:0,
-                joined: ''
-                
-            }
-        }
+        this.state = initialState;
     }
     loadUser = (data) => {
         this.setState({
@@ -120,6 +125,7 @@ class App extends Component {
                         .then(count => {
                             this.setState(Object.assign(this.state.user, { entries: count }))
                         })
+                        .catch(console.log);
                         
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
@@ -128,7 +134,7 @@ class App extends Component {
     }
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({ isSignedIn: false })
+            this.setState(initialState)
         } else if (route === 'home') {
             this.setState({isSignedIn: true})
         }
